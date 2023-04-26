b73168eedfb7fab29fa59f3c253a1971f9228b5b
rodrigozura <48832500+rodrigozura@users.noreply.github.com>
2023-04-26 07:59:47 -0300

    Challenge 2 solved

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
+//-----------------CHALLENGE 2 ----------------------
+
 const year = 1985;
 const holidays = [
   '01/01',
@@ -19,48 +21,41 @@ const holidays = [
 
 function countHours(year, holidays) {
   let count = 0;
-  const lastDay = `31/12/${year}`;
-  const firstDay = `01/01/${year}`;
   holidays.map((day) => {
     const completeDay = day.concat(`/${year}`);
     const date = new Date(completeDay);
-    //Comienzo y fin de año estan dentro del array
-    if (completeDay === lastDay && (date.getDay() !== 0 || date.getDay() !== 6))
-      count += 2;
-    if (
-      completeDay === firstDay &&
-      (date.getDay() !== 0 || date.getDay() !== 6)
-    )
-      count += 2;
-    if (date.getDay() === 0 || date.getDay() === 6) count += 2;
+    switch (date.getDay()) {
+      case 0:
+        console.log('Dia: ', date.getDay(), ' Domingo', '/ Contador: ', count);
+        break;
+      case 1:
+        console.log('Dia: ', date.getDay(), 'Lunes', '/ Contador: ', count);
+        count += 2;
+        break;
+      case 2:
+        console.log('Dia: ', date.getDay(), 'Martes', '/ Contador: ', count);
+        count += 2;
+        break;
+      case 3:
+        console.log('Dia: ', date.getDay(), 'Miercoles', '/ Contador: ', count);
+        count += 2;
+        break;
+      case 4:
+        console.log('Dia: ', date.getDay(), 'Jueves', '/ Contador: ', count);
+        count += 2;
+        break;
+      case 5:
+        console.log('Dia: ', date.getDay(), 'Viernes', '/ Contador: ', count);
+        count += 2;
+        break;
+      default:
+        console.log('Dia: ', date.getDay(), 'Sabado', '/ Contador: ', count);
+        break;
+    }
   });
-  count += 2;
 
   return count;
 }
-
 console.log(countHours(year, holidays));
 
-// switch (date.getDay()) {
-//   case 0:
-//     console.log("Dia: ",date.getDay()," Domingo","/ Contador: ", count)
-//     break;
-//   case 1:
-//     console.log("Dia: ",date.getDay(),"Lunes","/ Contador: ", count)
-//     break;
-//   case 2:
-//     console.log("Dia: ",date.getDay(),"Martes","/ Contador: ", count)
-//     break;
-//   case 3:
-//     console.log("Dia: ",date.getDay(),"Miercoles","/ Contador: ", count)
-//     break;
-//   case 4:
-//     console.log("Dia: ",date.getDay(),"Jueves","/ Contador: ", count)
-//     break;
-//   case 5:
-//     console.log("Dia: ",date.getDay(),"Viernes","/ Contador: ", count)
-//     break;
-//   default:
-//     console.log("Dia: ",date.getDay(),"Sabado","/ Contador: ", count)
-//     break;
-// }
+//---------------------------------------------------
